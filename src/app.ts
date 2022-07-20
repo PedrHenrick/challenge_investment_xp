5fbeed6a253bede58f46be24efa1de9cf53fd1ae
Pedro Mesquita <pedro.mesquita.hc@gmail.com>
2022-07-20 16:42:22 -0300

    feat: :sparkles: Adicionando cors no app

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,11 @@
 import 'express-async-errors';
-import express from 'express';
 import dotenv from 'dotenv';
+import express from 'express';
+import cors from 'cors'
 
 dotenv.config();
 const app = express();
-
+app.use(cors());
 app.use(express.json());
 
 export { app };
