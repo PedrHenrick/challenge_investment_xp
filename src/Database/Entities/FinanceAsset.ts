86408369e85cbca8b8353c73112797f9db48ae1b
Pedro Mesquita <pedro.mesquita.hc@gmail.com>
2022-07-20 21:45:55 -0300

    perf: :zap: Editando entidade para melhor criação da migration

--- src/Database/Entities/FinanceAsset.ts
+++ src/Database/Entities/FinanceAsset.ts
@@ -1,14 +1,20 @@
-import { Column, Entity, PrimaryColumn } from "typeorm";
+import { Column, CreateDateColumn, Entity, PrimaryColumn, UpdateDateColumn } from "typeorm";
 import { v4 as uuid } from 'uuid';
 
 @Entity('financial_assets')
 export class FinanceAsset {
-  @PrimaryColumn({ default: uuid() })
+  @PrimaryColumn({ type: 'uuid', default: uuid() })
   id: string;
 
-  @Column({ type: 'text', unique: true })
+  @Column({ type: 'varchar', unique: true })
   ativo: string;
   
-  @Column({ type: 'number' })
+  @Column({ type: 'integer' })
   valor: number;
+
+  @CreateDateColumn({ type: 'timestamp', default: 'now()' })
+  created_at: Date;
+
+  @UpdateDateColumn({ type: 'timestamp', default: 'now()' })
+  updated_at: Date;
 }
