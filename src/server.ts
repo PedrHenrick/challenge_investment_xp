d8ced44ffd18e0a02f46adc5cd886e6cb36068f3
Pedro Mesquita <pedro.mesquita.hc@gmail.com>
2022-07-21 02:34:32 -0300

    feat: :sparkles: Adicionando rotas e adicionando método cron

--- src/server.ts
+++ src/server.ts
@@ -2,7 +2,9 @@ import express from 'express';
 import 'express-async-errors';
 import { AppDataSource } from './Database/data-source';
 import cors from 'cors'
-import { router } from './Routers/router';
+import startCron from './cron/start-cron';
+import { DatabaseController } from './Controllers/database.controller';
+import { autoUpdateDatabaseController } from './Controllers/autoUpdateDatabase.controller';
 
 AppDataSource.initialize().then(() => {
   const PORT = process.env.PORT || 3000;
@@ -10,7 +12,11 @@ AppDataSource.initialize().then(() => {
 
   app.use(cors());
   app.use(express.json());
-  app.use(router);
+  app.get('/', new DatabaseController().all);
+  app.get('/update', new autoUpdateDatabaseController().autoUpdate);
 
-  return app.listen(PORT, () => console.log(`Server is running in ${ PORT }`));
+  return app.listen(PORT, () => {
+    console.log(`Server is running in ${ PORT }`);
+    startCron.run();
+  });
 });
