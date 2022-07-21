14d7027076386347fb7a0bcf83d2c51e2afdaefc
Pedro Mesquita <pedro.mesquita.hc@gmail.com>
2022-07-21 02:01:26 -0300

    perf: :zap: Apagando pasta e arquivo de rotas

--- src/Routers/router.ts
+++
@@ -1,8 +0,0 @@
-import express from 'express';
-import { DatabaseController } from '../Controllers/database.controller';
-
-const router = express.Router();
-
-router.get('/', new DatabaseController().all);
-
-export { router };
