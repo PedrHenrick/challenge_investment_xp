05a955a4eed1fb5444a1bd25971ab0064a58f0ca
Pedro Mesquita <pedro.mesquita.hc@gmail.com>
2022-07-21 00:05:53 -0300

    style: :art: Arrumando identação

--- src/Database/Entities/FinanceAsset.ts
+++ src/Database/Entities/FinanceAsset.ts
@@ -1,4 +1,10 @@
-import { Column, CreateDateColumn, Entity, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryGeneratedColumn,
+  UpdateDateColumn
+} from "typeorm";
 import { v4 as uuid } from 'uuid';
 
 @Entity('financial_assets')
--- src/Database/Migrations/1658372260612-create_financial_assets.ts
+++ src/Database/Migrations/1658372260612-create_financial_assets.ts
@@ -1,14 +1,13 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
 export class create_financial_assets1658372260612 implements MigrationInterface {
-    name = 'create_financial_assets1658372260612'
+  name = 'create_financial_assets1658372260612'
 
-    public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE "financial_assets" ("id" SERIAL NOT NULL, "ativo" character varying NOT NULL, "valor" integer NOT NULL, "created_at" TIMESTAMP NOT NULL DEFAULT 'now()', "updated_at" TIMESTAMP NOT NULL DEFAULT 'now()', CONSTRAINT "UQ_795c4726d697f2de02932d677d2" UNIQUE ("ativo"), CONSTRAINT "PK_89c7cd61eb5b0d86dc411ac0ee4" PRIMARY KEY ("id"))`);
-    }
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`CREATE TABLE "financial_assets" ("id" SERIAL NOT NULL, "ativo" character varying NOT NULL, "valor" integer NOT NULL, "created_at" TIMESTAMP NOT NULL DEFAULT 'now()', "updated_at" TIMESTAMP NOT NULL DEFAULT 'now()', CONSTRAINT "UQ_795c4726d697f2de02932d677d2" UNIQUE ("ativo"), CONSTRAINT "PK_89c7cd61eb5b0d86dc411ac0ee4" PRIMARY KEY ("id"))`);
+  }
 
-    public async down(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`DROP TABLE "financial_assets"`);
-    }
-
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`DROP TABLE "financial_assets"`);
+  }
 }
