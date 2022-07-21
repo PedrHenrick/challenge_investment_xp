0e4c17cd66df91039d3811a953fa244387e964f2
Pedro Mesquita <pedro.mesquita.hc@gmail.com>
2022-07-21 15:29:38 -0300

    fix: :bug: Adicionando variável de ambiente ao axios

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: 'http://localhost:3001/',
+  baseURL: process.env.ROUTE_AXIOS || 'http://localhost:3001',
 });
 
 export default api;
