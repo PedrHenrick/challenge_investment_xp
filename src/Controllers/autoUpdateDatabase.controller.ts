3adf121edaebe15ace1b0029030f9b5277de2341
Pedro Mesquita <pedro.mesquita.hc@gmail.com>
2022-07-21 03:45:57 -0300

    feat: :sparkles: Criando controller para autoUpdate do banco

--- src/Controllers/autoUpdateDatabase.controller.ts
+++ src/Controllers/autoUpdateDatabase.controller.ts
@@ -1,7 +1,18 @@
 import { Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import { AutoUpdateDatabaseService } from '../Services/autoUpdateDatabase.service';
 
 export class autoUpdateDatabaseController {
   async autoUpdate(_request: Request, response: Response) {
-    response.json("Lalalaland");
-  };
+    try {
+      const Database = new AutoUpdateDatabaseService();
+      const ActualizationMessage = await Database.autoUpdate();
+      return response.status(StatusCodes.OK).json(ActualizationMessage);
+    } catch(err) {
+      console.log(err);
+      return response
+        .status(StatusCodes.INTERNAL_SERVER_ERROR)
+        .json({ message: err });
+    }
+  }
 }
