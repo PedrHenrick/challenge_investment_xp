24937898b276cf3914dd36bbf28eac1cde1ec768
Pedro Mesquita <pedro.mesquita.hc@gmail.com>
2022-07-22 16:22:49 -0300

    feat: :sparkles: Criando função add no service  de registration e aplicando regra de negócio

---
+++ src/Services/registration.service.ts
@@ -0,0 +1,28 @@
+import { StatusCodes } from 'http-status-codes';
+import ErrorHandle from '../Class/error';
+import { UserRepository } from '../Database/Repositores/User.repository';
+import { RegistrationModel } from '../Models/registration.model';
+import { userType } from '../Types/User.type';
+import { generateJWTToken } from '../utils/JWTToken';
+import bcrypt from 'bcrypt-nodejs';
+
+export class RegistrationService {
+  async add(user: userType) {
+    const hasUser = await UserRepository.findOneBy({
+      email: user.email,
+    });
+
+    if(hasUser) throw new ErrorHandle(StatusCodes.BAD_REQUEST, 'User already exists')
+
+    const salt = bcrypt.genSaltSync(5);
+    user.password = bcrypt.hashSync(user.password, salt);
+    
+    const userModel = new RegistrationModel().add;
+    const userCreated = await userModel({
+      email: user.email,
+      password: user.password,
+    });
+    const token = generateJWTToken({...userCreated});
+    return token;
+  }
+};
