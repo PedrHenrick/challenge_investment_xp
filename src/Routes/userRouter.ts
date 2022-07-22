cab60c48167878116a84def9eed8fc748f5e1328
Pedro Mesquita <pedro.mesquita.hc@gmail.com>
2022-07-22 19:31:51 -0300

    feat: :sparkles: Adicionando rota /cliente/saque no arquivo de rota userRouter

--- src/Routes/userRouter.ts
+++ src/Routes/userRouter.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import { UserController } from '../Controllers/user.controller';
+import validateSchemaTransation from '../Middlewares/transationValidate.middeware';
 
 const userRouter = Router();
 
 userRouter.get('/:codCliente/saldo', new UserController().getBalance);
-// userRouter.post('/:codCliente/saque', new UserController().addWithdraw);
+userRouter.post('/saque', validateSchemaTransation, new UserController().addWithdraw);
 // userRouter.post('/:codCliente/deposito', new UserController().deposit);
 
 export { userRouter };
