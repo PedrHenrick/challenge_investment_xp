da2f92acf5c29f783e580e0742f78f23d4f8837e
Pedro Mesquita <pedro.mesquita.hc@gmail.com>
2022-07-22 12:50:24 -0300

    perf: :fire: Removendo todas as linhas de try/catch

--- src/Controllers/assets.controller.ts
+++ src/Controllers/assets.controller.ts
@@ -4,42 +4,21 @@ import { StatusCodes } from 'http-status-codes';
 
 export class AssetsController {
   async all(_request: Request, response: Response) {
-    try {
-      const assetsInstance = new AssetsService();
-      const allAssets = await assetsInstance.all();
-      return response.status(StatusCodes.OK).json(allAssets);
-    } catch(err) {
-      console.log(err);
-      return response
-        .status(StatusCodes.INTERNAL_SERVER_ERROR)
-        .json({ message: err });
-    }
+    const assetsInstance = new AssetsService();
+    const allAssets = await assetsInstance.all();
+    return response.status(StatusCodes.OK).json(allAssets);
   };
 
   async one(request: Request, response: Response) {
-    try {
-      const assetsInstance = new AssetsService();
-      const { codAtivo } = request.params;
-      const asset = await assetsInstance.one(Number(codAtivo));
-      return response.status(StatusCodes.OK).json(asset);
-    } catch(err) {
-      console.log(err);
-      return response
-        .status(StatusCodes.INTERNAL_SERVER_ERROR)
-        .json({ message: err });
-    }
+    const assetsInstance = new AssetsService();
+    const { codAtivo } = request.params;
+    const asset = await assetsInstance.one(Number(codAtivo));
+    return response.status(StatusCodes.OK).json(asset);
   }
 
   async autoUpdate(_request: Request, response: Response) {
-    try {
-      const assetsInstance = new AssetsService();
-      const ActualizationMessage = await assetsInstance.autoUpdate();
-      return response.status(StatusCodes.OK).json(ActualizationMessage);
-    } catch(err) {
-      console.log(err);
-      return response
-        .status(StatusCodes.INTERNAL_SERVER_ERROR)
-        .json({ message: err });
-    }
+    const assetsInstance = new AssetsService();
+    const ActualizationMessage = await assetsInstance.autoUpdate();
+    return response.status(StatusCodes.OK).json(ActualizationMessage);
   }
 }
