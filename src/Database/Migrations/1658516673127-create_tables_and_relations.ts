53bd06c466fbdadf8b9354c17c574e154630a73e
Pedro Mesquita <pedro.mesquita.hc@gmail.com>
2022-07-22 16:18:11 -0300

    fix: :bug: Refazendo migration
    Adicionando valor default no balance na tabela User e removendo a tag unique no password

--- src/Database/Migrations/1658516673127-create_tables_and_relations.ts
+++ src/Database/Migrations/1658516673127-create_tables_and_relations.ts
@@ -1,12 +1,12 @@
 import { MigrationInterface, QueryRunner } from "typeorm";
 
-export class create_tables_and_relations1658501384849 implements MigrationInterface {
-    name = 'create_tables_and_relations1658501384849'
+export class create_tables_and_relations1658516673127 implements MigrationInterface {
+    name = 'create_tables_and_relations1658516673127'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
-        await queryRunner.query(`CREATE TABLE "users_assets" ("client_code" SERIAL NOT NULL, "asset_code" SERIAL NOT NULL, CONSTRAINT "PK_8976bac0dc6c08e2a6618ac665b" PRIMARY KEY ("client_code", "asset_code"))`);
+        await queryRunner.query(`CREATE TABLE "users" ("client_code" SERIAL NOT NULL, "email" character varying NOT NULL, "password" character varying NOT NULL, "balance" numeric NOT NULL DEFAULT '0', CONSTRAINT "UQ_97672ac88f789774dd47f7c8be3" UNIQUE ("email"), CONSTRAINT "PK_ba4251b9c1795115a73e9040116" PRIMARY KEY ("client_code"))`);
         await queryRunner.query(`CREATE TABLE "financial_assets" ("asset_code" SERIAL NOT NULL, "name" character varying NOT NULL, "value" numeric NOT NULL, "amount_assets" integer NOT NULL, CONSTRAINT "UQ_91f3be094412baffe873fa6b7d1" UNIQUE ("name"), CONSTRAINT "PK_55a73f0a7f0f799e5ca56086de3" PRIMARY KEY ("asset_code"))`);
-        await queryRunner.query(`CREATE TABLE "users" ("client_code" SERIAL NOT NULL, "email" character varying NOT NULL, "password" character varying NOT NULL, "balance" numeric NOT NULL, CONSTRAINT "UQ_97672ac88f789774dd47f7c8be3" UNIQUE ("email"), CONSTRAINT "UQ_450a05c0c4de5b75ac8d34835b9" UNIQUE ("password"), CONSTRAINT "PK_ba4251b9c1795115a73e9040116" PRIMARY KEY ("client_code"))`);
+        await queryRunner.query(`CREATE TABLE "users_assets" ("client_code" SERIAL NOT NULL, "asset_code" SERIAL NOT NULL, CONSTRAINT "PK_8976bac0dc6c08e2a6618ac665b" PRIMARY KEY ("client_code", "asset_code"))`);
         await queryRunner.query(`ALTER TABLE "users_assets" ALTER COLUMN "client_code" DROP DEFAULT`);
         await queryRunner.query(`DROP SEQUENCE "users_assets_client_code_seq"`);
         await queryRunner.query(`ALTER TABLE "users_assets" ALTER COLUMN "asset_code" DROP DEFAULT`);
@@ -26,9 +26,9 @@ export class create_tables_and_relations1658501384849 implements MigrationInterf
         await queryRunner.query(`ALTER TABLE "users_assets" ALTER COLUMN "asset_code" SET DEFAULT nextval('"users_assets_asset_code_seq"')`);
         await queryRunner.query(`CREATE SEQUENCE IF NOT EXISTS "users_assets_client_code_seq" OWNED BY "users_assets"."client_code"`);
         await queryRunner.query(`ALTER TABLE "users_assets" ALTER COLUMN "client_code" SET DEFAULT nextval('"users_assets_client_code_seq"')`);
-        await queryRunner.query(`DROP TABLE "users"`);
-        await queryRunner.query(`DROP TABLE "financial_assets"`);
         await queryRunner.query(`DROP TABLE "users_assets"`);
+        await queryRunner.query(`DROP TABLE "financial_assets"`);
+        await queryRunner.query(`DROP TABLE "users"`);
     }
 
 }
