aa51ddde4aa0b6ddff078b545405a9053a6a3b8e
Pedro Mesquita <pedro.mesquita.hc@gmail.com>
2022-07-22 12:18:26 -0300

    feat: :sparkles: Adicionando função one na models
    Função responsável por retornar apenas um ativo

--- src/Models/assets.model.ts
+++ src/Models/assets.model.ts
@@ -1,19 +1,17 @@
 import { FinanceAssetRepository } from "../Database/Repositores/FinanceAsset.repository";
-
-type AssetsInformations = {
-  asset_code: number,
-  name?: string,
-  value: number,
-  amount_assets?: number
-}
+import { assetType } from "../Types/Assets.type";
 
 export class AssetsModel {
   async all() {
     const allAssets = await FinanceAssetRepository.find();
-    return allAssets as AssetsInformations[];
+    return allAssets as assetType[];
   }
 
-  async autoUpdate({asset_code, value}: AssetsInformations) {
+  async one({ asset_code, value }: assetType) {
+    const Assets = await FinanceAssetRepository.findOneBy({ asset_code });    return Assets as assetType;
+  }
+
+  async autoUpdate({asset_code, value}: assetType) {
     const Assets = await FinanceAssetRepository.findOneBy({ asset_code });
 
     if (!Assets) throw new Error('Assets deleted of database');
