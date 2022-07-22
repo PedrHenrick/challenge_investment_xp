d3f29cfe84229be7150b4cce4fa06149a6d9a85f
Pedro Mesquita <pedro.mesquita.hc@gmail.com>
2022-07-22 18:34:05 -0300

    feat: :sparkles: Adicionando arquivo de rota de /cliente no arquivo router

--- src/Routes/router.ts
+++ src/Routes/router.ts
@@ -2,14 +2,14 @@ import express from 'express';
 import authenticate from '../Middlewares/auth.middleware';
 import validateSchemaLogin from '../Middlewares/loginValidate.middeware';
 import { assertRouter } from './assertRouter';
-// import { userRouter } from './userRouter';
+import { userRouter } from './userRouter';
 import { loginRouter } from './loginRouter';
 import { registrationRouter } from './registrationRouter';
 
 const router = express.Router();
 
 router.use('/ativos', authenticate, assertRouter);
-// router.use('/cliente', authenticate, userRouter);
+router.use('/cliente', authenticate, userRouter);
 router.use('/login', validateSchemaLogin, loginRouter)
 router.use('/cadastro', validateSchemaLogin, registrationRouter);
 
