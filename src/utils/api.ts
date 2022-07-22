bc6d6523772fb8c5980279dd20a6679e72b7d76c
Pedro Mesquita <pedro.mesquita.hc@gmail.com>
2022-07-22 12:52:14 -0300

    fix: :bug: Alterando rota de requisição do axios para autoUpdate do banco

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: process.env.ROUTE_AXIOS || 'http://localhost:3001',
+  baseURL: process.env.ROUTE_AXIOS || 'http://localhost:3001/ativos/update',
 });
 
 export default api;
