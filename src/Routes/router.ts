6cc438ccf705b6e01205ae5dcf1b1e0559b13aad
Pedro Mesquita <pedro.mesquita.hc@gmail.com>
2022-07-22 15:10:34 -0300

    feat: :sparkles: Criando rota de /login e de /cliente e adicionando middleware de autenticação

--- src/Routes/router.ts
+++ src/Routes/router.ts
@@ -1,8 +1,13 @@
 import express from 'express';
-import { routerAtivos } from './routerAtivos';
+import authenticate from '../Middlewares/auth.middleware';
+import { assertRouter } from './assertRouter';
+import { clientRouter } from './clientRouter';
+import { loginRouter } from './loginRouter';
 
 const router = express.Router();
 
-router.use('/ativos', routerAtivos);
+router.use('/ativos', authenticate, assertRouter);
+router.use('/cliente/:codCliente', authenticate, clientRouter);
+router.use('/login', loginRouter)
 
 export { router };
