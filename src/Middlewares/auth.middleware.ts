8351d5b5f652f7568061afd1fc640f9f74372db4
Pedro Mesquita <pedro.mesquita.hc@gmail.com>
2022-07-22 15:05:52 -0300

    feat: :sparkles: Criando middleware de autenticação de rota

---
+++ src/Middlewares/auth.middleware.ts
@@ -0,0 +1,13 @@
+import { NextFunction, Request, Response } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import ErrorHandle from '../Class/error';
+import { authenticateToken } from '../utils/JWTToken';
+
+const authenticate = async (req: Request, _res: Response, next: NextFunction) => {
+  const token = req.headers.authorization;
+  if (!token) throw new ErrorHandle(StatusCodes.UNAUTHORIZED, 'Token not found');
+  await authenticateToken(token);
+  next();
+};
+
+export default authenticate;
