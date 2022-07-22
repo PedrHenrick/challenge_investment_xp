307d4567a2cdde955c46cd286ed7b61dace1565e
Pedro Mesquita <pedro.mesquita.hc@gmail.com>
2022-07-22 12:49:55 -0300

    feat: :sparkles: Adicionando middleware de erro no server

--- src/server.ts
+++ src/server.ts
@@ -4,6 +4,7 @@ import { AppDataSource } from './Database/data-source';
 import cors from 'cors'
 import startCron from './cron/start-cron';
 import { router } from './Routes/router';
+import ErrorMiddleware from './Middlewares/error.middleware';
 
 AppDataSource.initialize().then(() => {
   const PORT = process.env.PORT || 3000;
@@ -12,9 +13,10 @@ AppDataSource.initialize().then(() => {
   app.use(cors());
   app.use(express.json());
   app.use(router);
+  app.use(ErrorMiddleware);
 
   return app.listen(PORT, () => {
     console.log(`Server is running in ${ PORT }`);
-    startCron.run();
+    // startCron.run();
   });
 });
