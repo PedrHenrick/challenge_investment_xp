b9379151452c7394d43ba95876f751550e60033d
Pedro Mesquita <pedro.mesquita.hc@gmail.com>
2022-07-22 16:26:05 -0300

    feat: :sparkles: Adicionando rota cadastrar que chama o arquivo registrationRoute

--- src/Routes/router.ts
+++ src/Routes/router.ts
@@ -2,13 +2,15 @@ import express from 'express';
 import authenticate from '../Middlewares/auth.middleware';
 import validateSchemaLogin from '../Middlewares/loginValidate.middeware';
 import { assertRouter } from './assertRouter';
-import { clientRouter } from './clientRouter';
+// import { userRouter } from './userRouter';
 import { loginRouter } from './loginRouter';
+import { registrationRouter } from './registrationRouter';
 
 const router = express.Router();
 
 router.use('/ativos', authenticate, assertRouter);
-// router.use('/cliente/:codCliente', authenticate, clientRouter);
+// router.use('/cliente', authenticate, userRouter);
 router.use('/login', validateSchemaLogin, loginRouter)
+router.use('/cadastro', validateSchemaLogin, registrationRouter);
 
 export { router };
