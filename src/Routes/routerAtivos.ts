7d5592d5c214303ca0151ae1dcf0b379f1d2a970
Pedro Mesquita <pedro.mesquita.hc@gmail.com>
2022-07-22 12:37:09 -0300

    feat: :sparkles: Criando rota /ativos/:codAtivo

--- src/Routes/routerAtivos.ts
+++ src/Routes/routerAtivos.ts
@@ -4,6 +4,7 @@ import { AssetsController } from '../Controllers/assets.controller';
 const routerAtivos = Router();
 
 routerAtivos.get('/', new AssetsController().all);
+routerAtivos.get('/:codAtivo', new AssetsController().one);
 routerAtivos.get('/update', new AssetsController().autoUpdate);
 
 export { routerAtivos };
