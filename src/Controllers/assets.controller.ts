3a2af3688f7a878f9aa5cd0fea9350316a03639b
Pedro Mesquita <pedro.mesquita.hc@gmail.com>
2022-07-22 15:05:10 -0300

    feat: :sparkles: Adicionando tipo de retorno no controller da rota /ativos

--- src/Controllers/assets.controller.ts
+++ src/Controllers/assets.controller.ts
@@ -3,20 +3,20 @@ import { AssetsService } from "../Services/assets.service";
 import { StatusCodes } from 'http-status-codes';
 
 export class AssetsController {
-  async all(_request: Request, response: Response) {
+  async all(_request: Request, response: Response): Promise<Response> {
     const assetsInstance = new AssetsService();
     const allAssets = await assetsInstance.all();
     return response.status(StatusCodes.OK).json(allAssets);
   };
 
-  async one(request: Request, response: Response) {
+  async one(request: Request, response: Response): Promise<Response> {
     const assetsInstance = new AssetsService();
     const { codAtivo } = request.params;
     const asset = await assetsInstance.one(Number(codAtivo));
     return response.status(StatusCodes.OK).json(asset);
   }
 
-  async autoUpdate(_request: Request, response: Response) {
+  async autoUpdate(_request: Request, response: Response): Promise<Response> {
     const assetsInstance = new AssetsService();
     const ActualizationMessage = await assetsInstance.autoUpdate();
     return response.status(StatusCodes.OK).json(ActualizationMessage);
