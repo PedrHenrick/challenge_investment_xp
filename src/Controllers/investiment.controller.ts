050c1ccce75e9feda62eb2afe1879f87a024fe7d
Pedro Mesquita <pedro.mesquita.hc@gmail.com>
2022-07-23 00:43:22 -0300

    feat: :sparkles: Criando funcção buy no controller da rota /investimentos

---
+++ src/Controllers/investiment.controller.ts
@@ -0,0 +1,15 @@
+import { Response, Request } from 'express';
+import { StatusCodes } from 'http-status-codes';
+import { InvestimentService } from '../Services/investiment.service';
+import { authenticateToken } from '../utils/JWTToken';
+
+export class InvestimentController {
+  async buy(request: Request, response: Response): Promise<Response> {
+    const token = request.headers.authorization || '';
+    const userLogged = await authenticateToken(token);
+
+    const investimentService = new InvestimentService().buy;
+    const buyMade = await investimentService(userLogged, request.body);
+    return response.status(StatusCodes.CREATED).json({ message: buyMade });
+  }
+};
