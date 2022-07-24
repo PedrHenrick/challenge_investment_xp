1629763409e587593dafe7a51f6d07dd6a4b9702
Pedro Mesquita <pedro.mesquita.hc@gmail.com>
2022-07-23 21:11:12 -0300

    feat: :sparkles: Adicionando swagger no server

--- src/server.ts
+++ src/server.ts
@@ -2,9 +2,12 @@ import { AppDataSource } from './Database/data-source';
 import express from 'express';
 import 'express-async-errors';
 import cors from 'cors'
+import swaggerJSDoc from 'swagger-jsdoc';
+import swaggerUI from 'swagger-ui-express'
 import { router } from './Routes/router';
 import ErrorMiddleware from './Middlewares/error.middleware';
 import startCron from './cron/start-cron';
+import { swaggerConfig } from './docs/swagger.config';
 
 AppDataSource.initialize().then(() => {
   const PORT = process.env.PORT || 3000;
@@ -12,11 +15,13 @@ AppDataSource.initialize().then(() => {
 
   app.use(cors());
   app.use(express.json());
+  const swaggerDoc = swaggerJSDoc(swaggerConfig);
+  app.use('/docs', swaggerUI.serve, swaggerUI.setup(swaggerDoc));
   app.use(router);
   app.use(ErrorMiddleware);
 
   return app.listen(PORT, () => {
     console.log(`Server is running in ${ PORT }`);
-    startCron.run();
+    // startCron.run();
   });
 });
